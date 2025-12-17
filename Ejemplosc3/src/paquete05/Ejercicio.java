b04b7bf573a3cc82f3492b8d494f76a70d0e8c6b
CesarM3-A <cesarmateoab2007@gmail.com>
2025-12-17 12:54:58 -0500

    commit2

--- Ejemplosc3/src/paquete05/Ejercicio.java
+++ Ejemplosc3/src/paquete05/Ejercicio.java
@@ -21,13 +21,57 @@ public class Ejercicio {
         int[] produccionTotal = new int[5];
         int estacionProductiva = 0;
 
-        
         System.out.println("Ingrese los nombres de las 5 estaciones:");
         for (int i = 0; i < 5; i++) {
             System.out.printf("Nombre de la estación %d: ", i + 1);
             nombresEstaciones[i] = entrada.nextLine();
         }
 
-        
+        for (int i = 0; i < 5; i++) {
+            System.out.println("\nIngrese los nombres de los encargados de "
+                    + "cada estación:");
+            System.out.printf("Nombre del encargado de %s: ",
+                     nombresEstaciones[i]);
+            nombresEncargados[i] = entrada.nextLine();
+        }
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("\nIngrese la producción mensual de cada "
+                    + "estación (en dólares):");
+            System.out.printf("Producción mensual de %s:\n",
+                     nombresEstaciones[i]);
+            for (int j = 0; j < 12; j++) {
+                System.out.printf("Mes %d: ", j + 1);
+                produccionMensual[i][j] = entrada.nextInt();
+            }
+            entrada.nextLine();
+        }
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 12; j++) {
+                produccionTotal[i]
+                        = produccionTotal[i] + produccionMensual[i][j];
+            }
+        }
+
+        for (int i = 1; i < 5; i++) {
+            if (produccionTotal[i] > produccionTotal[estacionProductiva]) {
+                estacionProductiva = i;
+            }
+        }
+
+        System.out.println("\nAnálisis de Producción\n");
+        System.out.println("Estación\n");
+
+        for (int i = 0; i < 5; i++) {
+            System.out.printf("Nombre Estación: %s - Total Producción: $ %d\n",
+                     nombresEstaciones[i],
+                     produccionTotal[i]);
+        }
+
+        System.out.printf("\nEstación más productiva: %s\nEncargado de la "
+                + "estación: %s\n",
+                 nombresEstaciones[estacionProductiva],
+                 nombresEncargados[estacionProductiva]);
+
     }
 }
